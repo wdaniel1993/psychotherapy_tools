038cbd805d2da1a0679e0d874cd503a33bfc8254
Daniel Wallner <daniel.wallner@outlook.at>
2025-07-18 15:53:03 +0200

    Add command validators

---
+++ src/common/TherapyTools.Application.TherapyManagement/Commands/Validators/TherapyPlanCommandValidators.cs
@@ -0,0 +1,45 @@
+using FluentValidation;
+using TherapyTools.Application.TherapyManagement.Commands;
+using TherapyTools.Domain.TherapyManagement;
+
+namespace TherapyTools.Application.TherapyManagement.Commands.Validators;
+
+public class CreateTherapyPlanCommandValidator : AbstractValidator<CreateTherapyPlanCommand>
+{
+    public CreateTherapyPlanCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.GoalList.Goals).NotNull().NotEmpty();
+        RuleForEach(x => x.GoalList.Goals)
+            .ChildRules(goal =>
+            {
+                goal.RuleFor(g => g.Name).NotEmpty();
+                goal.RuleFor(g => g.Description).NotEmpty();
+            });
+        RuleFor(x => x.Description.Description).NotEmpty();
+    }
+}
+
+public class ActivateTherapyPlanCommandValidator : AbstractValidator<ActivateTherapyPlanCommand>
+{
+    public ActivateTherapyPlanCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+    }
+}
+
+public class CompleteTherapyPlanCommandValidator : AbstractValidator<CompleteTherapyPlanCommand>
+{
+    public CompleteTherapyPlanCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+    }
+}
+
+public class DiscardTherapyPlanCommandValidator : AbstractValidator<DiscardTherapyPlanCommand>
+{
+    public DiscardTherapyPlanCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+    }
+}
---
+++ src/common/TherapyTools.Application.TherapyManagement/Commands/Validators/TherapySessionCommandValidators.cs
@@ -0,0 +1,58 @@
+using FluentValidation;
+using TherapyTools.Application.TherapyManagement.Commands;
+using TherapyTools.Domain.TherapyManagement;
+using TherapyTools.Domain.TherapyManagement.ValueObjects;
+
+namespace TherapyTools.Application.TherapyManagement.Commands.Validators;
+
+public class ScheduleTherapySessionCommandValidator : AbstractValidator<ScheduleTherapySessionCommand>
+{
+    public ScheduleTherapySessionCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.SessionTimeSlot)
+            .NotNull()
+            .Must(slot => slot.Start < slot.End)
+            .WithMessage("Session start must be before end.");
+        RuleFor(x => x.Type).IsInEnum();
+        RuleFor(x => x.Notes.Notes).NotEmpty();
+    }
+}
+
+public class RescheduleTherapySessionCommandValidator : AbstractValidator<RescheduleTherapySessionCommand>
+{
+    public RescheduleTherapySessionCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.NewSlot)
+            .NotNull()
+            .Must(slot => slot.Start < slot.End)
+            .WithMessage("Session start must be before end.");
+    }
+}
+
+public class CancelTherapySessionCommandValidator : AbstractValidator<CancelTherapySessionCommand>
+{
+    public CancelTherapySessionCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+    }
+}
+
+public class CompleteTherapySessionCommandValidator : AbstractValidator<CompleteTherapySessionCommand>
+{
+    public CompleteTherapySessionCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.Notes.Notes).NotEmpty();
+    }
+}
+
+public class UpdateTherapySessionNotesCommandValidator : AbstractValidator<UpdateTherapySessionNotesCommand>
+{
+    public UpdateTherapySessionNotesCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.Notes.Notes).NotEmpty();
+    }
+}
